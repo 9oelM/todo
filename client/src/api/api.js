fc9bf3f8f7ad80e2a26577a1fc6f54ad818c73e7
9oelM <hj923@hotmail.com>
2019-05-22 09:40:23 +0000

    change todos to todo

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,13 +3,13 @@ import { SimpleFetch } from './util'
 
 const simpleFetch = new SimpleFetch('localhost:8888')
 
-const getTodos = () => simpleFetch.getMethod('todos')
+const getTodos = () => simpleFetch.getMethod('todo')
 
-const createTodo = todo => simpleFetch.postMethod('todos', todo)
+const createTodo = todo => simpleFetch.postMethod('todo', todo)
 
 const updateTodo = (todoId, todo) =>
-    simpleFetch.putMethod(`todos/${todoId}`, todo)
+    simpleFetch.putMethod(`todo/${todoId}`, todo)
 
-const deleteTodo = todoId => simpleFetch.deleteMethod('todos/${todoId}')
+const deleteTodo = todoId => simpleFetch.deleteMethod('todo/${todoId}')
 
 export { getTodos, createTodo, updateTodo, deleteTodo }
