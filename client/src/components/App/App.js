1efb90997a517b00336330b11f1b30ed9578e4ea
9oelM <hj923@hotmail.com>
2019-05-24 07:50:17 +0000

    Add Swipe

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,25 @@
 // External
 import React from 'react'
+import ReactSwipe from 'react-swipe'
 
 // Internal
 import TodoList from '../TodoList/TodoList'
 import './App.scss'
 
 const App = () => {
+    let reactSwipeEl
     return (
         <div className="App">
-            <div id="todo-container">
-                <TodoList />
-            </div>
+            <ReactSwipe
+                className="carousel"
+                swipeOptions={{ continuous: false }}
+                ref={el => (reactSwipeEl = el)}
+            >
+                <div id="todo-container">
+                    <TodoList handleSwipe={() => reactSwipeEl.next()} />
+                </div>
+                <div id="todo-editor" />
+            </ReactSwipe>
         </div>
     )
 }
