2b4b170eb5ba2b64ae80f0dc362429e3400d722d
9oelM <hj923@hotmail.com>
2019-05-25 01:14:43 +0000

    Test api

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,11 @@
 // External
-import React from 'react'
-import ReactSwipe from 'react-swipe'
+import React, { useEffect } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 // Internal
+import { getTodos } from '../../api/api'
 import TodoList from '../TodoList/TodoList'
 import TodoEditor from '../TodoEditor/TodoEditor'
 import './App.scss'
@@ -20,6 +20,15 @@ const App = () => {
         slidesToScroll: 1,
         className: 'app-slider',
     }
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getTodos()
+            console.log(result)
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className="App">
             <Slider ref={slider => (sliderElement = slider)} {...settings}>
