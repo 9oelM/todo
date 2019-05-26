979c75222d4883b6bff50fdff50db9aff9ed8aa0
9oelM <hj923@hotmail.com>
2019-05-26 11:10:50 +0000

    Experiment with notifying past dues

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,20 +2,56 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import moment from 'moment'
 
 // Internal
 import { getAndCatchError } from '../../util/api'
+import { onInfo } from '../Notifier/Notifier'
 import TodoList from '../TodoList/TodoList'
 import TodoEditor from '../TodoEditor/TodoEditor'
 import Button from '../Button/Button'
 import './App.scss'
+
+const notifyTodosDueExpiry = todos => {
+    let alreadyNotified = []
+    let timeouts = []
+
+    const innerLogic = () => {
+        if (todos && todos.length !== 0) {
+            todos.forEach(({ _id, title, due, isNotified, ...rest }) => {
+                const notify = () => {
+                    onInfo(
+                        `Todo "${title}" has reached its due date (${moment(
+                            due
+                        ).format()}). Please check.`
+                    )
+                    alreadyNotified = [...alreadyNotified, _id]
+                }
+                if (due !== '') {
+                    if (
+                        due < new Date().getTime() &&
+                        !alreadyNotified.includes(_id)
+                    )
+                        notify()
+                    else if (due > new Date().getTime())
+                        timeouts = [
+                            ...timeouts,
+                            setTimeout(notify(), due - new Date().getTime()),
+                        ]
+                }
+            })
+            return timeouts
+        }
+        return []
+    }
+    return innerLogic()
+}
 
 const App = () => {
     const [rootState, setRootState] = useState({
         todos: [],
         isLoading: false,
     })
-
     const [updateUtil, setUpdateUtil] = useState(0)
     const setUpdateUtilInChild = () =>
         setUpdateUtil(val => (val % 2 ? val + 1 : val - 1))
@@ -32,8 +68,19 @@ const App = () => {
                 todos,
                 isLoading: false,
             }))
+            return notifyTodosDueExpiry(todos)
         }
-        fetchData()
+        const notifyTodosDueExpirySubscription = fetchData()
+
+        return function cleanup() {
+            if (
+                notifyTodosDueExpirySubscription &&
+                notifyTodosDueExpirySubscription.length > 0
+            )
+                notifyTodosDueExpirySubscription.forEach(timeout =>
+                    clearTimeout(timeout)
+                )
+        }
     }, [updateUtil])
 
     const TodoListComponent = () => (
