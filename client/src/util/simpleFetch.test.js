be00a476f70e41de55c3b9109b1c301a2b77ba25
9oelM <hj923@hotmail.com>
2019-05-26 11:16:06 +0000

    remove useless files

--- client/src/util/simpleFetch.test.js
+++
@@ -1,9 +0,0 @@
-import { SimpleFetch } from './util'
-
-describe('SimpleFetch', () => {
-    const confusingUrl = 'https://test.url/'
-    const simpleFetch = new SimpleFetch(confusingUrl)
-
-    it('removes a dangling / of URL at the end', () =>
-        expect(simpleFetch.baseUrl.substr(-1)).not.toBe('/'))
-})
